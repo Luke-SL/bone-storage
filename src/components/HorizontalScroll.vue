c7faaf2eec361601f217a68d2bf3f9ce9f9b45f8
Lucas <lukeedward2022@gmail.com>
2024-09-12 15:42:59 -0300

    compoent horizontal scroll and bug cut card fixed

---
+++ src/components/HorizontalScroll.vue
@@ -0,0 +1,177 @@
+<template>
+  <div class="scroll-container">
+    <!-- Scroll Area with Horizontal Scroll -->
+    <div
+      class="scroll-area"
+      ref="scrollArea"
+      @mousedown="startDrag"
+      @mousemove="onDrag"
+      @mouseup="stopDrag"
+      @mouseleave="stopDrag"
+    >
+      <div class="scroll-content">
+        <q-card
+          v-for="item in heavyList"
+          :key="item.id"
+          class="item-card"
+          flat
+          borderless
+        >
+          <q-img :src="item.image" class="item-image" />
+          <q-card-section>
+            <div class="row no-wrap items-center">
+              <div class="col text-h6 ellipsis">{{ item.name }}</div>
+            </div>
+          </q-card-section>
+          <q-card-section class="q-pt-none">
+            Por
+            <div class="text-subtitle1 text-bold">
+              {{ formatCurrency(item.price) }}
+            </div>
+            <div>
+              6x de
+              <span class="text-subtitle1 text-bold">{{
+                formatCurrency(item.part)
+              }}</span>
+              sem juros
+            </div>
+          </q-card-section>
+          <q-card-actions>
+            <q-btn outline color="amber-9">Comprar</q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
+    </div>
+
+    <!-- Navigation Buttons -->
+    <div class="scroll-controls" v-if="$q.platform.is.desktop">
+      <q-btn
+        class="scroll-control scroll-left"
+        round
+        dense
+        flat
+        color="orange"
+        text-color="black"
+        icon="mdi-chevron-left"
+        @click="scrollLeftFunc"
+      />
+      <q-btn
+        class="scroll-control scroll-right"
+        round
+        dense
+        flat
+        color="orange"
+        text-color="black"
+        icon="mdi-chevron-right"
+        @click="scrollRight"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+import { formatCurrency } from "src/utils/format";
+
+export default {
+  name: "HorizontalScroll",
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props) {
+    const scrollArea = ref(null);
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
+
+    const startDrag = (e) => {
+      isDragging = true;
+      startX = e.pageX - scrollArea.value.getBoundingClientRect().left;
+      scrollLeft = scrollArea.value.scrollLeft;
+    };
+
+    const onDrag = (e) => {
+      if (!isDragging) return;
+      const x = e.pageX - scrollArea.value.getBoundingClientRect().left;
+      const walk = (x - startX) * 2; // Scroll-fast multiplier
+      scrollArea.value.scrollLeft = scrollLeft - walk;
+    };
+
+    const stopDrag = () => {
+      isDragging = false;
+    };
+
+    const scrollLeftFunc = () => {
+      if (scrollArea.value) {
+        scrollArea.value.scrollBy({ left: -300, behavior: "smooth" });
+      }
+    };
+
+    const scrollRight = () => {
+      if (scrollArea.value) {
+        scrollArea.value.scrollBy({ left: 300, behavior: "smooth" });
+      }
+    };
+
+    const heavyList = computed(() => props.list);
+
+    return {
+      scrollArea,
+      scrollLeftFunc,
+      scrollRight,
+      startDrag,
+      onDrag,
+      stopDrag,
+      heavyList,
+      formatCurrency,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.scroll-container {
+  position: relative;
+  width: 100%; /* Garante que o contêiner use toda a largura disponível */
+  overflow: hidden; /* Evita que o conteúdo extra seja visível fora do contêiner */
+}
+
+.scroll-area {
+  display: flex; /* Garante que os itens sejam exibidos em linha */
+  overflow-x: hidden; /* Permite o scroll horizontal */
+  white-space: nowrap; /* Impede a quebra de linha */
+  padding: 0 10px; /* Adiciona padding para evitar o corte dos itens */
+}
+
+.scroll-content {
+  display: flex;
+}
+
+.item-card {
+  flex: 0 0 auto; /* Garante que os itens não encolham */
+  margin: 0 10px; /* Margem entre os itens */
+}
+
+.item-image {
+  max-height: 200px;
+  height: auto;
+  width: 200px;
+}
+
+.scroll-controls {
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  transform: translateY(-50%);
+}
+
+.scroll-control {
+  pointer-events: auto;
+  z-index: 1; /* Garante que os botões estejam sobre o conteúdo */
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
-  <q-page class="">
-    <div class="">
+  <q-page>
+    <div>
       <q-carousel
         swipeable
         animated
@@ -77,100 +77,9 @@
     <h1 class="flex flex-center text-bold text-h5 q-my-xl">
       NOVOS LANÇAMENTOS
     </h1>
-
-    <!--<q-virtual-scroll
-      :items="heavyList"
-      virtual-scroll-horizontal
-      v-slot="{ index, item }"
-    >
-      <q-card class="q-mx-md q-mt-md" flat borderless :key="index">
-        <q-img
-          :src="item.image"
-          style="max-height: 150px; height: 100%; width: 200px; max-width: 100%"
-        />
-
-        <q-card-section>
-          <div class="row no-wrap items-center">
-            <div class="col text-h6 ellipsis">{{ item.name }}</div>
-            <div
-              class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-            ></div>
-          </div>
-
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          Por
-          <div class="text-subtitle1 text-bold">
-            {{ formatCurrency(item.price) }}
-          </div>
-          <div>
-            6x de
-            <span class="text-subtitle1 text-bold">
-              {{ formatNotDisplayCurrency(item.part) }}
-            </span>
-            sem juros
-          </div>
-        </q-card-section>
-
-        <q-card-actions>
-          <q-btn outline="" color="amber-9"> Comprar </q-btn>
-        </q-card-actions>
-      </q-card>
-    </q-virtual-scroll>-->
-    <div>
-      <q-scroll-area style="height: 375px">
-        <div class="row no-wrap">
-          <q-card
-            v-for="item in heavyList"
-            class="q-mx-md q-mt-md"
-            flat
-            borderless
-            :key="item"
-          >
-            <q-img
-              :src="item.image"
-              style="
-                max-height: 150px;
-                height: 100%;
-                width: 200px;
-                max-width: 100%;
-              "
-            />
-
-            <q-card-section>
-              <div class="row no-wrap items-center">
-                <div class="col text-h6 ellipsis">{{ item.name }}</div>
-                <div
-                  class="col-auto text-grey text-caption q-pt-md row no-wrap items-center"
-                ></div>
-              </div>
-              <!--
-          <q-rating v-model="item.rating" :max="5" size="32px" />
-          -->
-            </q-card-section>
-
-            <q-card-section class="q-pt-none">
-              Por
-              <div class="text-subtitle1 text-bold">
-                {{ formatCurrency(item.price) }}
-              </div>
-              <div>
-                6x de
-                <span class="text-subtitle1 text-bold">
-                  {{ formatNotDisplayCurrency(item.part) }}
-                </span>
-                sem juros
-              </div>
-            </q-card-section>
-
-            <q-card-actions>
-              <q-btn outline="" color="amber-9"> Comprar </q-btn>
-            </q-card-actions>
-          </q-card>
-        </div>
-      </q-scroll-area>
-    </div>
+    <HorizontalScroll :list="releaseList" />
+    <h1 class="flex flex-center text-bold text-h5 q-my-xl">MAIS PEDIDOS</h1>
+    <HorizontalScroll :list="mostWantedList" />
     <q-page-sticky position="bottom-right" :offset="[18, 65]">
       <q-btn fab icon="mdi-whatsapp" color="green" />
     </q-page-sticky>
@@ -179,18 +88,17 @@
 
 <script>
 import { ref } from "vue";
-import { formatCurrency, formatNotDisplayCurrency } from "src/utils/format";
-import { figuresList, heavyList } from "./assets/tables";
+import { figuresList, releaseList, mostWantedList } from "./assets/tables";
+import HorizontalScroll from "src/components/HorizontalScroll.vue";
 
 export default {
+  components: { HorizontalScroll },
   setup() {
     return {
       slide: ref(1),
       figuresList,
-
-      heavyList,
-      formatCurrency,
-      formatNotDisplayCurrency,
+      releaseList,
+      mostWantedList,
     };
   },
 };
