1c5a1c03598096d549e70a470ae6faf7ddc47558
Lucas <lukeedward2022@gmail.com>
2024-11-12 15:07:51 -0300

    added lateral image

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,26 +1,23 @@
 <template>
   <q-page>
-    <div class="q-gutter-md row items-start flex flex-center">
-      <q-img src="bone-produto/1.png" style="height: 560px; max-width: 600px" />
+    <div class="q-gutter-md row items-start flex flex-center q-">
+      <q-img
+        src="https://images.tcdn.com.br/img/img_prod/868639/bone_trucker_islandia_465_1_efa334c05f05d5e1b9fdd74883363227.jpg"
+        spinner-color="white"
+        style="height: 140px; max-width: 150px; border: 1px black solid"
+        class="q-mb-lg"
+      />
+      <q-img
+        src="https://images.tcdn.com.br/img/img_prod/868639/bone_trucker_islandia_465_1_efa334c05f05d5e1b9fdd74883363227.jpg"
+        style="height: 560px; max-width: 600px"
+        class=""
+      />
       <div class="q-ml-xl">
-        <p class="text-h4 ellipsis">Boné Bege N</p>
+        <p class="text-h4 ellipsis">Boné Truck</p>
         <p class="text-weight-bold ellipsis">Código do produto: 1234</p>
         <p class="text-weight-thin ellipsis">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </p>
-        <q-card class="my-card">
-          <q-card-section>
-            <div class="row">
-              <q-icon size="xl" name="mdi-credit-card" color="grey" />
-              <p>R$ 159,00</p>
-            </div>
-            <q-btn color="teal">
-              <q-icon left size="3em" name="mdi-cart-outline" />
-
-              <div>COMPRAR</div>
-            </q-btn>
-          </q-card-section>
-        </q-card>
       </div>
     </div>
   </q-page>
