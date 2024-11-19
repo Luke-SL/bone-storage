a51b5f899d2824906db6b6d3ea3db9a24aa1e677
Lucas <lukeedward2022@gmail.com>
2024-11-19 19:42:53 -0300

    chatgpt code

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,23 +1,56 @@
 <template>
   <q-page>
-    <div class="q-gutter-md row items-start flex flex-center q-">
-      <q-img
-        src="https://images.tcdn.com.br/img/img_prod/868639/bone_trucker_islandia_465_1_efa334c05f05d5e1b9fdd74883363227.jpg"
-        spinner-color="white"
-        style="height: 140px; max-width: 150px; border: 1px black solid"
-        class="q-mb-lg"
-      />
-      <q-img
-        src="https://images.tcdn.com.br/img/img_prod/868639/bone_trucker_islandia_465_1_efa334c05f05d5e1b9fdd74883363227.jpg"
-        style="height: 560px; max-width: 600px"
-        class=""
-      />
-      <div class="q-ml-xl">
-        <p class="text-h4 ellipsis">Boné Truck</p>
-        <p class="text-weight-bold ellipsis">Código do produto: 1234</p>
-        <p class="text-weight-thin ellipsis">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </p>
+    <div class="row flex flex-center">
+      <div class="column col-1 q-ml-xl">
+        <q-img
+          src="https://inspiringfans4takes.vteximg.com.br/arquivos/ids/193733-100-100/21544-024_1-Bone-Oficial-Equipe-2022-F1-Mercedes-Benz-Branco.jpg?v=638302133713270000"
+          spinner-color="white"
+          style="max-height: 100px; max-width: 100px; border: 1px grey solid"
+          class="col q-mb-sm"
+        />
+        <q-img
+          src="https://inspiringfans4takes.vteximg.com.br/arquivos/ids/193734-100-100/21544-024_2-Bone-Oficial-Equipe-2022-F1-Mercedes-Benz-Branco.jpg?v=638302133852830000"
+          spinner-color="white"
+          style="max-height: 100px; max-width: 100px; border: 1px grey solid"
+          class="col q-mb-sm"
+        />
+        <q-img
+          src="https://inspiringfans4takes.vteximg.com.br/arquivos/ids/193735-100-100/21544-024_3-Bone-Oficial-Equipe-2022-F1-Mercedes-Benz-Branco.jpg?v=638302133989300000"
+          spinner-color="white"
+          style="max-height: 100px; max-width: 100px; border: 1px grey solid"
+          class="col q-mb-sm"
+        />
+        <q-img
+          src="https://inspiringfans4takes.vteximg.com.br/arquivos/ids/193736-100-100/21544-024_4-Bone-Oficial-Equipe-2022-F1-Mercedes-Benz-Branco.jpg?v=638302134181070000"
+          spinner-color="white"
+          style="max-height: 100px; max-width: 100px; border: 1px grey solid"
+          class="col q-mb-sm"
+        />
+      </div>
+      <div
+        class="q-gutter-md row items-start flex flex-center col-5 q-ml-xl row no-wrap q-mt-lg"
+        style="max-width: 800px"
+      >
+        <q-img
+          src="https://inspiringfans4takes.vteximg.com.br/arquivos/ids/193733-800-800/21544-024_1-Bone-Oficial-Equipe-2022-F1-Mercedes-Benz-Branco.jpg?v=638302133713270000"
+          style="height: 550px; width: 500px"
+          class=""
+        />
+        <div class="q-ml-xl col-5">
+          <p class="text-h4 ellipsis">Boné Truck</p>
+          <p class="text-weight-bold ellipsis">Código do produto: 1234</p>
+          <p class="text-weight-thin ellipsis">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          </p>
+          <p>
+            <q-btn
+              color="secondary"
+              label="Comprar"
+              icon="mdi-cart"
+              class="q-pa-md"
+            />
+          </p>
+        </div>
       </div>
     </div>
   </q-page>
